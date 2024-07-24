e30dc2e2f2efc0b03a2a8dba7749a07834ca4c67
LincolnHubbard <hubbard.lincoln@gmail.com>
2024-07-23 17:36:27 -0700

    begin mockup of task edit window

--- src/display.js
+++ src/display.js
@@ -3,22 +3,43 @@ import { Manager } from "./manager";
 export class DisplayController{
 
     constructor(){
+        this.manager = new Manager();
+
+        //find DOM elements
         this.newProjectButton = document.querySelector('#newProjectButton');
         this.newTaskButton = document.querySelector('#newTaskButton');
+        this.projectList = document.querySelector('#project-list');
+        this.taskList = document.querySelector('#task-list');
+
         this.initializeDisplay();
     }
 
     initializeDisplay(){
         if(this.newProjectButton){
             console.log("New project button found");
+            this.newProjectButton.addEventListener('click', () =>{
+                this.createProjectWindow();
+            })
         }
 
         if(this.newTaskButton){
             console.log("New task button found");
-            this.newProjectButton.addEventListener('click', () =>{
+            this.newTaskButton.addEventListener('click', () =>{
                 this.createTaskWindow();
             })
         }
+
+        if(this.projectList){
+            console.log("Project list found");
+        }
+
+        if(this.taskList){
+            console.log("Task list found");
+        }
+    }
+    
+    createProjectWindow(){
+        console.log("Creating new project");
     }
 
     createTaskWindow(){
